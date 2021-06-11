cc7f479deab1918e8b24533379565a966afa233a
phamvi00 <phamvi00@users.noreply.github.com>
2021-06-11 14:08:35 -0400

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/meetVi')
+def meetVi():
+    return render_template('meetVi.html', title="Vi Pham", url=os.getenv("URL"))
+
+@app.route('/meetAshley')
+def meetAshley():
+    return render_template('meetAshley.html', title="Ashley Ye", url=os.getenv("URL"))
