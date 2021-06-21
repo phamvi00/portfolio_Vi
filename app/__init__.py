926c5e73723853377eeff7215496a01742fb3d13
phamvi00 <phamvi00@users.noreply.github.com>
2021-06-21 12:48:02 -0400

    new

--- app/__init__.py
+++ app/__init__.py
@@ -8,15 +8,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="Vi Pham's Portfolio", url=os.getenv("URL"))
 
-@app.route('/meetVi')
-def meetVi():
-    return render_template('meetVi.html', title="Vi Pham", url=os.getenv("URL"))
-
-@app.route('/meetAshley')
-def meetAshley():
-    return render_template('meetAshley.html', title="Ashley Ye", url=os.getenv("URL"))
 
 if __name__ ==  "__main__":
     app.run(debug=True)
