9604076d1b572601f32d17ebbbec4bf6f69b477c
phamvi00 <phamvi00@users.noreply.github.com>
2021-07-11 16:16:48 -0400

    auth

--- app/__init__.py
+++ app/__init__.py
@@ -42,45 +42,37 @@ def register():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
 
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
             return f"User {username} created successfully"
         else:
             return error, 418
 
     ## TODO: Return a restister page
-    return "Register Page not yet implemented", 501
+    return render_template('register.html')
 
 @app.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+        user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
             error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
+        elif not check_password_hash(user.password, password):
             error = 'Incorrect password.'
 
         if error is None:
@@ -89,7 +81,7 @@ def login():
             return error, 418
 
     ## TODO: Return a login page
-    return "Login Page not yet implemented", 501
+    return render_template('login.html')
 
 if __name__ ==  "__main__":
     app.run(debug=True)
